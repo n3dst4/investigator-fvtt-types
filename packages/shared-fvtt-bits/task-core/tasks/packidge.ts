abf048939d0ca9d23d6d3357f2fd0f2e5c0710cb
Neil de Carteret <git@lumphammer.net>
2024-07-05 18:27:05 +0100

    git subrepo pull (merge) packages/shared-fvtt-bits
    subrepo:
      subdir:   "packages/shared-fvtt-bits"
      merged:   "a0b03c9"
    upstream:
      origin:   "git@github.com:n3dst4/shared-fvtt-bits.git"
      branch:   "main"
      commit:   "a0b03c9"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

--- packages/shared-fvtt-bits/task-core/tasks/packidge.ts
+++ packages/shared-fvtt-bits/task-core/tasks/packidge.ts
@@ -3,36 +3,36 @@ import chalk from "chalk";
 import fs from "fs-extra";
 import path from "path";
 
-import { TaskArgs } from "../types";
+import { Manifest, TaskArgs } from "../types";
 
 /**
  * create a releasable package
  * (package is a reserved word)
  */
-export async function packidge({ manifest, buildPath, log }: TaskArgs) {
+export async function packidge({
+  manifest,
+  buildPath,
+  log,
+  packagePath = "package",
+}: TaskArgs) {
   return new Promise<void>((resolve, reject) => {
-    try {
-      // Ensure there is a directory to hold all the packaged versions
-      fs.ensureDirSync("package");
-      // Initialize the zip file
-      const zipName = process.env["ZIP_FILE_NAME"] ?? `${manifest.id}.zip`;
-      const zipFile = fs.createWriteStream(path.join("package", zipName));
-      const zip = archiver("zip", { zlib: { level: 9 } });
-      zipFile.on("close", () => {
-        log(chalk.green(zip.pointer() + " total bytes"));
-        log(chalk.green(`Zip file ${zipName} has been written`));
-        return resolve();
-      });
-      zip.on("error", (err: any) => {
-        throw err;
-      });
-      zip.pipe(zipFile);
-      // Add the directory with the final code
-      zip.directory(buildPath, manifest.id);
-      zip.finalize();
-    } catch (err) {
-      return reject(err);
-    }
+    const id = (manifest as Manifest).id;
+    // Ensure there is a directory to hold all the packaged versions
+    fs.ensureDirSync(packagePath);
+    // Initialize the zip file
+    const zipName = process.env["ZIP_FILE_NAME"] ?? `${id}.zip`;
+    const zipFile = fs.createWriteStream(path.join(packagePath, zipName));
+    const zip = archiver("zip", { zlib: { level: 9 } });
+    zipFile.on("close", () => {
+      log(chalk.green(zip.pointer() + " total bytes"));
+      log(chalk.green(`Zip file ${zipName} has been written`));
+      resolve();
+    });
+    zip.on("error", reject);
+    zip.pipe(zipFile);
+    // Add the directory with the final code
+    zip.directory(buildPath, id);
+    zip.finalize().catch(reject);
   });
 }
 
